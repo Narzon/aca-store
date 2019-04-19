5aed0127a6d0b9520ebada5c1dcd0cd61858c34a
Nicolai A <nantonov@utexas.edu>
2019-04-18 19:13:08 -0500

    more modular design

--- public/index.js
+++ public/index.js
@@ -2,14 +2,6 @@
 //Nicolai Antonov
 //April 2019
 
-//create variables in sessionStorage to keep track of shopping cart if they don't currently exist
-if (sessionStorage.getItem('myCart') == undefined) {
-    sessionStorage.setItem('myCart', '');
-}
-if (sessionStorage.getItem('cartValue') == undefined) { 
-    sessionStorage.setItem('cartValue', '');
-}
-
 //load main products page
 //if value exists in searchbox, filter to only include results with that value
 //else, if category is selected, filter to show only products in that category
@@ -20,6 +12,14 @@ let loadPage = () => {
     statusbar.innerHTML = " "
     let searchTerm = document.getElementById("searchbox").value
     let catSelect = document.getElementById("catSelect").value
+    //create cart in sessionStorage if it doesn't yet exist
+    //create variables in sessionStorage to keep track of shopping cart if they don't currently exist
+    if (sessionStorage.getItem('myCart') == undefined) {
+        sessionStorage.setItem('myCart', '');
+    }
+    if (sessionStorage.getItem('cartValue') == undefined) { 
+        sessionStorage.setItem('cartValue', '');
+    }
     let newStr = ""
     if (searchTerm) {
         for (let i = 0; i < products.length; i++) {
@@ -44,88 +44,6 @@ let loadPage = () => {
     //place products in container to display all products in a grid
     newStr = '<div id="container">' + newStr + '</div>' 
     currentDiv.innerHTML = newStr
-}
-
-//define function to remove item from cart and subtract its value from the current total
-let removeItem = (i) => {
-    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
-    let cartValue = Number(sessionStorage.getItem('cartValue'))
-    let itemValue = 0
-    //create an empty string to record removed item's value
-    let tempStr = ""
-    //parse string of removed item backwards until reaching $ sign to determine value
-    for (let j = cart[i].length - 1; j > 0; j --) {
-        if (cart[i][j] === "$") {
-            tempStr = cart[i].substr(j + 1)
-        }
-    }
-    itemValue = Number(tempStr)
-    cartValue -= itemValue
-    cart.splice(i, 1);
-    sessionStorage.setItem('myCart', convertStringsArrays(cart));
-    sessionStorage.setItem('cartValue', "" + cartValue);
-    showCart()
-}
-
-//define function to add a product to cart based on its passed in ID, and add its value to the cart total
-let addToCart = (id) => {
-    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
-    if (cart[0] === "") {
-        cart[0] = (products[id - 1].name + "  - " + products[id - 1].price)
-    } else {
-        cart.push(products[id - 1].name + "  - " + products[id - 1].price)
-    }
-    let cartValue = Number(sessionStorage.getItem('cartValue'))
-    cartValue += Number(products[id - 1].price.substring(1))
-    sessionStorage.setItem('myCart', convertStringsArrays(cart));
-    sessionStorage.setItem('cartValue', "" + cartValue);
-    //empty status bar
-    let statusbar = document.getElementById("statusbar")
-    statusbar.innerHTML = `<font color="red">${products[id - 1].name} added to cart!</font>` 
-}
-
-//define function to show each item in the cart and the total value
-let showCart = () => {
-    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
-    let cartValue = Number(sessionStorage.getItem('cartValue'))
-    let cartStr = ""
-    if (cart.length === 1 && cart[0] === "") {
-        cartStr += `<p>Cart is Empty!</p>`
-    } else {
-        for (let i = 0; i < cart.length; i++) {
-            cartStr += `<p> ${cart[i]} <button type="button" onclick="removeItem(${i})">Remove</button></p>`
-        }
-    }
-    cartStr += `<h3>Total price: $${cartValue.toFixed([2])}</h3><button type="button" onclick="checkout()">Checkout</button><br><br><button type="button" onclick="loadPage()">Return</button>`
-    currentDiv.innerHTML = cartStr
-    //empty status bar
-    let statusbar = document.getElementById("statusbar")
-    statusbar.innerHTML = " "
-}
-
-//define a function to show checkout screen
-let checkout = () => {
-    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
-    //if cart is empty, alert with an error
-    if (cart.length === 1 && cart[0] === "") {
-        alert("Error! Empty cart.")
-        return
-    }
-    let cartValue = Number(sessionStorage.getItem('cartValue'))
-    let checkoutStr = ""
-    checkoutStr += `<form>
-            First Name: <input></input><br>
-            Last Name: <input></input><br>
-            E-mail Address: <input></input><br>
-            <h2>Total price: $${cartValue.toFixed([2])}</h2>
-            <button type="submit">Place Order</button>
-            <br><br>
-            <button type="button" onclick="loadPage()">Return</button>
-        </form>`
-    currentDiv.innerHTML = checkoutStr
-    //empty status bar
-    let statusbar = document.getElementById("statusbar")
-    statusbar.innerHTML = " "
 }
 
 //define a function to show product description on page, reached on clicking its image from products page
---
+++ public/shopCart.js
@@ -0,0 +1,81 @@
+//define function to remove item from cart and subtract its value from the current total
+let removeItem = (i) => {
+    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
+    let cartValue = Number(sessionStorage.getItem('cartValue'))
+    let itemValue = 0
+    //create an empty string to record removed item's value
+    let tempStr = ""
+    //parse string of removed item backwards until reaching $ sign to determine value
+    for (let j = cart[i].length - 1; j > 0; j --) {
+        if (cart[i][j] === "$") {
+            tempStr = cart[i].substr(j + 1)
+        }
+    }
+    itemValue = Number(tempStr)
+    cartValue -= itemValue
+    cart.splice(i, 1);
+    sessionStorage.setItem('myCart', convertStringsArrays(cart));
+    sessionStorage.setItem('cartValue', "" + cartValue);
+    showCart()
+}
+
+//define function to add a product to cart based on its passed in ID, and add its value to the cart total
+let addToCart = (id) => {
+    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
+    if (cart[0] === "") {
+        cart[0] = (products[id - 1].name + "  - " + products[id - 1].price)
+    } else {
+        cart.push(products[id - 1].name + "  - " + products[id - 1].price)
+    }
+    let cartValue = Number(sessionStorage.getItem('cartValue'))
+    cartValue += Number(products[id - 1].price.substring(1))
+    sessionStorage.setItem('myCart', convertStringsArrays(cart));
+    sessionStorage.setItem('cartValue', "" + cartValue);
+    //empty status bar
+    let statusbar = document.getElementById("statusbar")
+    statusbar.innerHTML = `<font color="red">${products[id - 1].name} added to cart!</font>` 
+}
+
+//define function to show each item in the cart and the total value
+let showCart = () => {
+    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
+    let cartValue = Number(sessionStorage.getItem('cartValue'))
+    let cartStr = ""
+    if (cart.length === 1 && cart[0] === "") {
+        cartStr += `<p>Cart is Empty!</p>`
+    } else {
+        for (let i = 0; i < cart.length; i++) {
+            cartStr += `<p> ${cart[i]} <button type="button" onclick="removeItem(${i})">Remove</button></p>`
+        }
+    }
+    cartStr += `<h3>Total price: $${cartValue.toFixed([2])}</h3><button type="button" onclick="checkout()">Checkout</button><br><br><button type="button" onclick="loadPage()">Return</button>`
+    currentDiv.innerHTML = cartStr
+    //empty status bar
+    let statusbar = document.getElementById("statusbar")
+    statusbar.innerHTML = " "
+}
+
+//define a function to show checkout screen
+let checkout = () => {
+    let cart = convertStringsArrays(sessionStorage.getItem('myCart'))
+    //if cart is empty, alert with an error
+    if (cart.length === 1 && cart[0] === "") {
+        alert("Error! Empty cart.")
+        return
+    }
+    let cartValue = Number(sessionStorage.getItem('cartValue'))
+    let checkoutStr = ""
+    checkoutStr += `<form>
+            First Name: <input></input><br>
+            Last Name: <input></input><br>
+            E-mail Address: <input></input><br>
+            <h2>Total price: $${cartValue.toFixed([2])}</h2>
+            <button type="submit">Place Order</button>
+            <br><br>
+            <button type="button" onclick="loadPage()">Return</button>
+        </form>`
+    currentDiv.innerHTML = checkoutStr
+    //empty status bar
+    let statusbar = document.getElementById("statusbar")
+    statusbar.innerHTML = " "
+}
